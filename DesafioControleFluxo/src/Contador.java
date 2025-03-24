cb60532343ad7181316609f477fd2bb38a59503d
geraldo <geraldoaguiar68@outlook.com>
2025-03-24 12:40:01 -0300

    DesafioControleFluxo

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,9 +1,5 @@
 
-
 import java.util.Scanner;
-
-
-
 
 public class Contador {
 
@@ -11,75 +7,67 @@ public class Contador {
    * O programa recebe dois números do usuário.
    * Valida se o o primeiro número , é maior que o segundo.
    * Se a validação for falsa, ele entra na exceção e imprimi o erro.
-   * Se a validação for verdadeira ele imprime um contador com base na diferença entre o primeiro e o segundo número. 
+   * Se a validação for verdadeira ele imprime um contador com base na diferença
+   * entre o primeiro e o segundo número.
    * 
    * @param args
    */
 
+  public static void main(String[] args) {
 
-
-    public static void main(String[] args){ 
-         
-          Scanner terminal=new Scanner(System.in);
-          
-          System.out.println("Digite o primeiro Número: ");
-          int parametroUm=terminal.nextInt();
-
-          System.out.println("Digite o segundo Número: ");
-          int parametroDois=terminal.nextInt();
-
-          
-
-              
-                  try{
-                    contar(parametroUm, parametroDois);
-                  }
-
-            catch(Exception e){
-                    
-              
-              System.out.println(e.getMessage());
-            }
-            }
-          
+    try (Scanner terminal = new Scanner(System.in)) {
       
-      
-    
-    
-    static void contar(int numeroUm, int numeroDois) throws Exception{
-             // Validar se parametroUm é maior que parametroDois e lançar exceção
+      // Solicita ao usuário o valor do primeiro parâmetro.
+      System.out.println("Digite o primeiro Número: ");
+      int parametroUm = terminal.nextInt();
 
-               
-               if(numeroDois > numeroUm){
-                
-                int contagem=(numeroDois - numeroUm);
+      // Solicita ao usuário o valor do segundo parâmetro.
+      System.out.println("Digite o segundo Número: ");
+      int parametroDois = terminal.nextInt();
 
-                for(int i = 1;contagem > 0;i++ ){
-               
-                  //realiza o for para imprimir os números com base na variável contagem.
-                  System.out.println("Imprimindo o número "+ i);
+      // Pula uma linha depois da interação com o usuário.
+      System.out.println();
 
-                  contagem-=1;
-                  }
-                  
+      try {
+        contar(parametroUm, parametroDois);
+      }
 
-                    }
-                else{
-                   
-                  throw new Exception("O segundo Número deve ser maior que o primeiro !");
-                }
+      catch (Exception e) {
+        e.getMessage();
 
-
-          System.out.println("\nFIM DO PROGRAMA !");
-         }
-        
-   
+      }
     }
-              
+  }
 
-              
+  /**
+   * @param parametroUm   . Recebe o primeiro número do usuário.
+   * @param parametroDois . Recebe o segundo número do usuário.
+   * @throws ParametrosInvalidosException
+   */
+  static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
 
-             
+    // Valida se parametroUm é maior que parametroDois e lançar exceção
+    if (parametroDois > parametroUm) {
 
-    
+      int contagem = (parametroDois - parametroUm);
 
+      for (int i = 1; contagem > 0; i++) {
+
+        // realiza o for para imprimir os números com base na variável contagem.
+        System.out.println("Imprimindo o número " + i);
+
+        contagem--;
+      }
+
+      // Imprime mensagem de 'Programa Finalizado', caso a condição seja verdadeira.
+     System.out.println("\nPROGRAMA FINALIZADO...!\n");
+
+    } else {
+
+      // Caso a condição seja falsa, o programa lança a exceção e imprime a mensagem.
+      throw new ParametrosInvalidosException();
+    }
+
+  }
+
+}
--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,7 +1,13 @@
 
 
-public class ParametrosInvalidosException {
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(){
+
+        System.out.println("O segundo número, deve ser maior que o primeiro !\n");
+        
+
+    }
     
   
-      
 }
